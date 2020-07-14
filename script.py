b5d735f503a21ddd08dcca78c68aa75ebf650615
Leon Koole <leon@koole.io>
2020-07-14 21:05:46 +0200

    Format script.py

--- script.py
+++ script.py
@@ -1,10 +1,11 @@
 import bpy
 import random
 
-o  = bpy.data.objects['Plane'] # Replace with your actual object's name
-t  = o.active_material.node_tree
-noise = t.nodes['Noise Texture']
-noise.inputs['Scale'].default_value = random.randint(15, 30) / 100
+canvas = bpy.data.objects["Plane"]
+tree = canvas.active_material.node_tree
 
-mapping = t.nodes['Group']
-mapping.inputs['Value'].default_value = random.randint(0, 1000)
+noise = tree.nodes["Noise Texture"]
+noise.inputs["Scale"].default_value = random.randint(15, 30) / 100
+
+mapping = tree.nodes["Group"]
+mapping.inputs["Value"].default_value = random.randint(0, 1000)
